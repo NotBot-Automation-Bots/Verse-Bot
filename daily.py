7d33fbdc9a32e47efcc8ad2734edb6923b3b84ca
Hanzala Sohrab <sorb.hanzala@gmail.com>
2021-08-08 20:29:23 +0530

    Fixed bugs in daily scheduler

--- daily.py
+++ daily.py
@@ -10,15 +10,15 @@ bot = Bot(ACCESS_TOKEN)
 
 # Database config
 client = MongoClient(foo.MONGO_URI)
-db_operations = client.user
+db_operations = client.FbMessenger.user
 
 db_collections = {}
 
-db_collections['IamLoved'] = client.IamLoved
-db_collections['IamOk'] = client.IamOk
-db_collections['IamSafe'] = client.IamSafe
-db_collections['LaborAndDelivery'] = client.LaborAndDelivery
-db_collections['LetsDoThis'] = client.LetsDoThis
+db_collections['IamLoved'] = client.FbMessenger.IamLoved
+db_collections['IamOk'] = client.FbMessenger.IamOk
+db_collections['IamSafe'] = client.FbMessenger.IamSafe
+db_collections['LaborAndDelivery'] = client.FbMessenger.LaborAndDelivery
+db_collections['LetsDoThis'] = client.FbMessenger.LetsDoThis
 
 scheduler = BlockingScheduler()
 
