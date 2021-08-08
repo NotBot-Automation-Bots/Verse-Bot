b8dd40078278b532d0364c442a4bc84ba37cc61b
Hanzala Sohrab <sorb.hanzala@gmail.com>
2021-08-08 12:18:40 +0530

    Added scheduler

---
+++ daily.py
@@ -0,0 +1,34 @@
+from pymongo import MongoClient
+import foo
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from pymessenger.bot import Bot
+
+ACCESS_TOKEN = foo.ACCESS_TOKEN
+VERIFY_TOKEN = foo.VERIFY_TOKEN
+bot = Bot(ACCESS_TOKEN)
+
+# Database config
+client = MongoClient(foo.MONGO_URI)
+db_operations = client.user
+
+db_collections = {}
+
+db_collections['IamLoved'] = client.IamLoved
+db_collections['IamOk'] = client.IamOk
+db_collections['IamSafe'] = client.IamSafe
+db_collections['LaborAndDelivery'] = client.LaborAndDelivery
+db_collections['LetsDoThis'] = client.LetsDoThis
+
+scheduler = BackgroundScheduler()
+
+@scheduler.scheduled_job("interval", seconds=10)
+def daily():
+    users = db_operations.find({"schedule": "Daily"})
+    for user in users:
+        _id = user["_id"]
+        audio_url = f"https://verse-recordings.s3.ap-south-1.amazonaws.com/{user['_id']}/{user['ref']}.mp3"
+        # bot.send_audio_url(recipient_id=_id, audio_url=audio_url)
+        bot.send_text_message(recipient_id=_id, message="Hello")
+
+scheduler.start()
