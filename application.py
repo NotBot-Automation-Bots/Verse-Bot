013fb0d1d8a277534bbc7cb4e091a9013fc8e970
Hanzala Sohrab <sorb.hanzala@gmail.com>
2021-08-11 11:11:26 +0530

    Testing

--- application.py
+++ application.py
@@ -136,6 +136,7 @@ def receive_message():
     else:
         # get whatever message a user sent the bot
         output = request.get_json()
+        print(output)
         for event in output['entry']:
             messaging = event['messaging']
             for message in messaging:
